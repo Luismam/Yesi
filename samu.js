473614f81a34a44a8d5b6b643266d5fb94136ed8
Luismam <88789875+Luismam@users.noreply.github.com>
2021-08-12 13:20:47 -0400

    Update samu.js

--- samu.js
+++ samu.js
@@ -950,7 +950,7 @@ case 'help':
 case 'menu':
 case 'comandos':
 
-rede = '*😊Seamos amigos en facebook!!*'
+redes = '*😊Seamos amigos en facebook!!*'
 opcion = redes[Math.floor(Math.random() * redes.length)]
 
 var num = sam.participant
